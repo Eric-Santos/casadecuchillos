5ac78e63d2207aef0ac2f63dea1752e58fd7290f
Eric Santos <eric355402@yahoo.com>
2022-06-20 04:03:30 -0600

    added intro and gallery sections with generic video and images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,8 @@
 import images from './images';
 import meal from '../assets/meal.mp4';
+// new video added below
+import grill from '../assets/grill.webm';
+import steak from '../assets/steak.webm';
 import data from './data';
 
-export { images, meal, data };
+export { images, meal, grill, steak, data };
